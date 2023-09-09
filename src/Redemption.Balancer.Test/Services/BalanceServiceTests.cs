2ec2315ab685e0d62aca1ad3714ce64e0bd391de
Abolfazl Moslemian <moslemianabolfazl@gmail.com>
2023-09-09 14:49:40 +0330

    add Test for balance service

--- src/Redemption.Balancer.Test/Services/BalanceServiceTests.cs
+++ src/Redemption.Balancer.Test/Services/BalanceServiceTests.cs
@@ -1,8 +1,8 @@
 ﻿using FluentAssertions;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Moq;
 using Redemption.Balancer.Api.Application.Common.Contracts;
 using Redemption.Balancer.Api.Application.Common.Exceptions;
+using Redemption.Balancer.Api.Application.Common.Models.Balances;
 using Redemption.Balancer.Api.Application.Common.Models.Dtos.Balances;
 using Redemption.Balancer.Api.Constants;
 using Redemption.Balancer.Api.Domain.Entities;
@@ -29,19 +29,152 @@ public class BalanceServiceTests
     {
         var request = new BalanceStatusInputDto()
         {
-
+            B2BIRRRate = 40000
         };
 
         var accountId = Account.MasterId;
 
         var cancellationToken = CancellationToken.None;
 
-        var expectedResult = new BalanceStatusOutputDto()
+        var transactions = new List<TransactionEntity>()
         {
+            new TransactionEntity()
+            {
+               FromAccountId = accountId,
+                ToAccountId= 1001,
+                Symbol = "USDT",
+                Amount = 1000,
+                TotalValue = 1000,
+                CreatedAt = new DateTime(2023,08,28)
 
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1001,
+                ToAccountId=accountId,
+                Symbol = "IRR",
+                Amount = 50000000,
+                TotalValue = 1000,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId = 1002,
+                Symbol = "USDT",
+                Amount = 500,
+                TotalValue = 500,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1002,
+                ToAccountId=accountId,
+                Symbol = "IRR",
+                Amount = 15000000,
+                TotalValue = 300,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId= 1003,
+                Symbol = "USDT",
+                Amount = 100,
+                TotalValue = 100,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId= 1003,
+                Symbol = "IRR",
+                Amount = 15000000,
+                TotalValue = 300,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1004,
+                ToAccountId= accountId,
+                Symbol = "DOGE",
+                Amount = 1000,
+                TotalValue = 60m,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+             new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId= 1004,
+                Symbol = "IRR",
+                Amount = 3000000,
+                TotalValue = 60,
+                CreatedAt = new DateTime(2023,08,28)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId = 1001,
+                Symbol = "IRR",
+                Amount = 20000000,
+                TotalValue = 333.333m,
+                CreatedAt = new DateTime(2023,08,29)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1001,
+                ToAccountId = accountId,
+                Symbol = "USDT",
+                Amount = 400,
+                TotalValue = 400,
+                CreatedAt = new DateTime(2023,08,29)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1002,
+                ToAccountId = accountId,
+                Symbol = "IRR",
+                Amount = 30000000,
+                TotalValue = 600,
+                CreatedAt = new DateTime(2023,08,29)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = 1002,
+                ToAccountId = accountId,
+                Symbol = "USDT",
+                Amount = 100,
+                TotalValue = 100,
+                CreatedAt = new DateTime(2023,08,29)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId = 1003,
+                Symbol = "IRR",
+                Amount = 20000000,
+                TotalValue = 500,
+                CreatedAt = new DateTime(2023,08,29)
+            },
+            new TransactionEntity()
+            {
+                FromAccountId = accountId,
+                ToAccountId = 1003,
+                Symbol = "USDT",
+                Amount = 400,
+                TotalValue = 400,
+                CreatedAt = new DateTime(2023,08,29)
+            },
         };
 
-        var transactions = new List<TransactionEntity>() { new TransactionEntity() };
+        var expectedResult = new BalanceStatus()
+        {
+            IRRGained = -20600000m,
+            USDTGained = -515,
+            IRRInject = -47300000,
+            USDTInject = 1182.5m,
+        };
+
 
         _transactionService.Setup(ts => ts.GetAccountTransactions(accountId, cancellationToken, null, null)).ReturnsAsync(transactions);
 

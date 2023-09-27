6d62df871ba23c2584046c816e57f6496aee3447
Abolfazl Moslemian <moslemianabolfazl@gmail.com>
2023-09-27 18:49:00 +0330

    add migration for Numeric to Decimal

---
+++ src/Redemption.Balancer.Api/Infrastructure/Persistence/Migrations/20230927151427_NumericToDecimal.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Redemption.Balancer.Api.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NumericToDecimal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalValue",
+                table: "Transactions",
+                type: "numeric(24,10)",
+                precision: 24,
+                scale: 10,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "numeric(24,10)",
+                precision: 24,
+                scale: 10,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Value",
+                table: "AccountConfigs",
+                type: "numeric(24,10)",
+                precision: 24,
+                scale: 10,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalValue",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(24,10)",
+                oldPrecision: 24,
+                oldScale: 10);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "Transactions",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(24,10)",
+                oldPrecision: 24,
+                oldScale: 10);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Value",
+                table: "AccountConfigs",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(24,10)",
+                oldPrecision: 24,
+                oldScale: 10);
+        }
+    }
+}
